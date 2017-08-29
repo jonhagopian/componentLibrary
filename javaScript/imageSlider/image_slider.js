faf8eb08f5df1aa2eed020a73a624c1136d07a0e
Jon Hagopian <jon.hagopian@gmail.com>
2017-08-28 23:54:12 -0400

    updated css for image sizing and JS for resize timer

--- javaScript/imageSlider/image_slider.js
+++ javaScript/imageSlider/image_slider.js
@@ -4,29 +4,24 @@
   Attach imageSlider() to load event.
   If window is wider than total width of all slides, script will justify slides without animation.
   Animation is percentage based.
-
   slide center, distance from SCROLLER container left side
   slide center, position IN CONTAINER
   convert to percentage, this is the value to move images within slide right
   move image to center using 1/2 image width, 50%
-
+  setTimout on resize to detect on resize complete
   To slow or speed image lag see: sPosPct = sPosPct * 0.20, 1 is full, 0 none
-
   Images should be large enough to fill lag distance within slide
-
-  Future additions: Previous/Next buttons to clue user?
-
   note: let not var, to prevent value from being replaced
   
+  Future additions: Previous/Next buttons to clue user?
+    on resize event listener is added again, cannot use removeEventListener due to scope and arguments
+
   Collect Data--
   allSliders = all gallery sliders in the document
   box = the parent container block that contains the scrolling gallery
   boxW = box width
-  s0 = first slide, used to reference
-  sW = width of the individual slides in gallery
-  sMR = width of the margin used on each slide, currently margin right
-  sF = width of the individual slide plus margin right
   sArr = array containing all slide objects within gallery
+  sW = width of the individual slides in gallery
   sOffsetArr = array containing the pixel offset of each slide
   sImgArr = array containing each slides image object 
 
@@ -43,13 +38,20 @@
 function imageSlider(firstRun) {
   if (firstRun === true) {
     // Re-init slider
-    window.addEventListener("resize", imageSlider);
+    var resizeDone;
+    window.addEventListener("resize", function() {
+      clearTimeout(resizeDone);
+      resizeDone = setTimeout(function() {
+        imageSlider();
+      }, 200);
+    });
     window.addEventListener("orientationchange", imageSlider);
   }
-  function sAnimate(box, boxW, sW, sMR, sOffsetArr, sImgArr) {
+  function sAnimate(box, boxW, sW, sOffsetArr, sImgArr) {
+    console.log("animated");
     var scrPos = box.scrollLeft;
     for (var i = 0; i < sOffsetArr.length; i++) {
-      var sPos = sOffsetArr[i] + ((sW+sMR) / 2);
+      var sPos = sOffsetArr[i] + (sW / 2);
       var sPosVis = scrPos - sPos;
       var sPosPct = (sPosVis / boxW) * 100;
       sPosPct = sPosPct + 50; // plus 50% for center
@@ -62,28 +64,26 @@ function imageSlider(firstRun) {
   for (var j = 0; j < allSliders.length; j++) {
     let box = allSliders[j];
     let boxW = box.offsetWidth;
-    let s0 = box.querySelector("div");
-    let sW = s0.offsetWidth;
-    let sMR = parseFloat(getComputedStyle(s0).marginRight);
-    let sF = sW + sMR;
-    let sArr = box.querySelectorAll(".image_slide");
+    let sArr = box.querySelectorAll("figure");
+    let sW = box.querySelector("figure").offsetWidth;
+    let sOffsetArr = [];
+    sArr.forEach(function(item){
+      sOffsetArr.push(item.offsetLeft)
+    });
+    let sImgArr = box.querySelectorAll("figure img");
     // If screen is too wide, no need to run slider
-    if (sF * (sArr.length - 1) <= boxW) {
+    if (box.scrollWidth <= boxW) {
       box.setAttribute("class","image_slider justified");
       box.scrollLeft = 0;
+      for (var i = 0; i < sImgArr.length; i++) {
+        sImgArr[i].style.transform = "translateX(0%)";
+      }
     } else {
       box.setAttribute("class","image_slider");
-      let sOffsetArr = [];
-      let sImgArr = [];
-      // For each individual slide within the parent slider
-      for (var i = 0; i < sArr.length; i++) {
-        sOffsetArr.push(sArr[i].offsetLeft);
-        sImgArr.push(sArr[i].querySelector("img"));
-      }
       box.addEventListener("scroll", function() {
-        sAnimate(box, boxW, sW, sMR, sOffsetArr, sImgArr);
+        sAnimate(box, boxW, sW, sOffsetArr, sImgArr);
       });
-      sAnimate(box, boxW, sW, sMR, sOffsetArr, sImgArr);
+      sAnimate(box, boxW, sW, sOffsetArr, sImgArr);
     }
   }
 } //EOF
