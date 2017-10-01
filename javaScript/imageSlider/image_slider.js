1fb40d06252c5e65dd0eb32d73b6966858cd1f7f
Jon Hagopian <jon.hagopian@gmail.com>
2017-09-30 22:53:43 -0400

    almost done refining

--- javaScript/imageSlider/image_slider.js
+++ javaScript/imageSlider/image_slider.js
@@ -2,41 +2,29 @@
   READ ME
   Multiple instances of slider may exist. Add css class 'image_slider' to gallery container.
   Attach imageSlider() to load event.
-  If window is wider than total width of all slides, script will justify slides without animation.
+  Mobile gallery is horizontal, tablet and larger gallery is vertical.
   Animation is percentage based.
-  slide center, distance from SCROLLER container left side
-  slide center, position IN CONTAINER
-  convert to percentage, this is the value to move images within slide right
-  move image to center using 1/2 image width, 50%
+  convert to percentage, this is the value to move images within slide right or down
+  move image to center using 1/2 image width/height, 50%
   To slow or speed image lag see: sPosPct = sPosPct * 0.20, 1 is full, 0 none
   Images should be large enough to fill lag distance within slide
-  note: let not var, to prevent value from being replaced
+  note: let not var, to prevent value from being replaced within block
   event listeners: '_forEventListener' solves the issue of passing parameters and removing event
-  set add/remove event in same block
   
   Collect Data--
   allSliders = all gallery sliders in the document
-  container = the parent container block that contains everything
-  box = the container block that contains the scrolling gallery
-  boxW = box width
+  box = the block element that contains the scrolling gallery items
   sArr = array containing all slide objects within gallery
-  sW = width of the individual slides in gallery
-  sWm = width plus margin, used for desktop navigation
-  sOffsetArr = array containing the pixel offset of each slide
-  sImgArr = array containing each slides image object 
+  sImgArr = array containing each slides image object
+  boxW/H box width/height
+  sW/H = width/height of the individual slides in gallery
+  sOffsetArrT/L = array containing the pixel offset of each slide
 
   Calculate on Scroll for each slide-- sAnimate()
   sPos = current slide position in box
   sPosVis = current slide position in visible portion of box
   sPosPct = convert position to percent, then apply to image
   scrPos = scroll position (position box is scrolled from TL zero)
-
-  Mobile first, Desktop nav works on changing scroll via timer on click.
-  timeOut = global conatiner for desktop nav values
-  target = directs move() as to what gallery is assigned
-  btnLeft, btnRigh = navigation elements
-
-  See read me in styles.css
 */
 
 // Image Gallery Slider
@@ -47,64 +35,27 @@ function imageSlider(firstRun) {
       clearTimeout(resizeDone);
       resizeDone = setTimeout(function() {
         imageSlider();
-      }, 200);
+      }, 400);
     });
     window.addEventListener("orientationchange", imageSlider);
   }
-  // Desktop Navigation
-  var timeOut = [];
-  function move(box, sWm, sImgArr, direction, btnL, btnR, target) {
-    if (direction === "right") {
-      var scrollRt = box.scrollLeft + sWm;
-      function moveScrollRt() {
-        if (box.scrollLeft < scrollRt) {
-          box.scrollLeft += 8;
-          timeOut[target][1] = "moving";
-          timeOut[target][0] = setTimeout(moveScrollRt, 10);
-        } else {
-          timeOut[target][1] = "done";
-          return;
-        }
-      }
-      if (box.scrollLeft === (sImgArr.length * sWm) - sWm * 2) {
-        btnR.style.display = "none"; // hide btn after click of 2nd to last
-      } else {
-        btnL.removeAttribute("style");
-      }
-      if (timeOut[target][1] !== "done") {
-        return;
-      } else {
-        moveScrollRt();
-      }
-    } else if (direction === "left") {
-      var scrollLt = box.scrollLeft - sWm;
-      function moveScrollLt() {
-        if (box.scrollLeft > scrollLt) {
-          box.scrollLeft -= 8;
-          timeOut[target][1] = "moving";
-          timeOut[target][0] = setTimeout(moveScrollLt, 10);
-        } else {
-          timeOut[target][1] = "done";
-          return;
-        }
-      }
-      if (box.scrollLeft === sWm) {
-        btnL.style.display = "none"; // hide btn after back click on first
-      } else {
-        btnR.removeAttribute("style");
-      }
-      if (timeOut[target][1] !== "done") {
-        return;
-      } else {
-        moveScrollLt();
-      }
+  function syAnimate(box, boxH, sH, sOffsetArrT, sImgArr) {
+    var scrPos = box.scrollTop;
+    // for each individual image slide
+    for (var i = 0; i < sOffsetArrT.length; i++) {
+      var sPos = sOffsetArrT[i] + (sH / 2);
+      var sPosVis = scrPos - sPos;
+      var sPosPct = (sPosVis / boxH) * 100; // get the percentage to apply to image inside
+      sPosPct = sPosPct + 50; // plus 50% for center
+      sPosPct = (sPosPct * 0.20).toFixed(2); // change image movement lag by reducing the %
+      sImgArr[i].style.transform = "translateY(" + sPosPct + "%)";
     }
   } //EOF
-  function sAnimate(box, boxW, sW, sOffsetArr, sImgArr) {
+  function sxAnimate(box, boxW, sW, sOffsetArrL, sImgArr) {
     var scrPos = box.scrollLeft;
     // for each individual image slide
-    for (var i = 0; i < sOffsetArr.length; i++) {
-      var sPos = sOffsetArr[i] + (sW / 2);
+    for (var i = 0; i < sOffsetArrL.length; i++) {
+      var sPos = sOffsetArrL[i] + (sW / 2);
       var sPosVis = scrPos - sPos;
       var sPosPct = (sPosVis / boxW) * 100; // get the percentage to apply to image inside
       sPosPct = sPosPct + 50; // plus 50% for center
@@ -112,24 +63,43 @@ function imageSlider(firstRun) {
       sImgArr[i].style.transform = "translateX(" + sPosPct + "%)";
     }
   } //EOF
-  var allSliders = document.querySelectorAll(".image-slider-container");
-  // For each individual gallery
-  for (var j = 0; j < allSliders.length; j++) {
-    let container = allSliders[j];
-    let box = container.querySelector("div[class='image-slider']");
-    let boxW = box.offsetWidth;
+  var allSliders = document.querySelectorAll(".image-slider");
+  for (var j = 0; j < allSliders.length; j++) { // For each individual gallery element
+    let box = allSliders[j]; // let so values aren't overwritten
     let sArr = box.querySelectorAll("figure");
-    let sW = box.querySelector("figure").offsetWidth;
-    let sWm = box.querySelector("figure").offsetWidth + parseInt(getComputedStyle(box.querySelector("figure")).marginRight); // with margins
-    let sOffsetArr = [];
-    for (var i = 0; i < sArr.length; i++) {
-      sOffsetArr.push(sArr[i].offsetLeft);
-    }
     let sImgArr = box.querySelectorAll("figure img");
-    box.setAttribute("class","image-slider");
-    let _forEventListener = function() {
-      sAnimate(box, boxW, sW, sOffsetArr, sImgArr);
-    }
+    // if horizontal/vertical scroll switch
+    var flexDir = window.getComputedStyle(box).flexDirection;
+    if (flexDir === "row") {
+      box.scrollLeft = 0; // reset scroll to beginning
+      var boxW = box.offsetWidth;
+      var sW = box.querySelector("figure").offsetWidth;
+      var sOffsetArrL = [];
+      for (var i = 0; i < sArr.length; i++) {
+        sOffsetArrL.push(sArr[i].offsetLeft);
+        sArr[i].removeAttribute("style");
+      }
+      var _forEventListener = function() {
+        sxAnimate(box, boxW, sW, sOffsetArrL, sImgArr);
+      }
+      sxAnimate(box, boxW, sW, sOffsetArrL, sImgArr);
+    } else if (flexDir === "column") {
+      box.scrollTop = 0;
+      var boxH = box.offsetHeight;
+      var sH = box.querySelector("figure").offsetHeight;
+      var sOffsetArrT = [];
+      for (var i = 0; i < sArr.length; i++) {
+        sOffsetArrT.push(sArr[i].offsetTop);
+        // Aspect ratio adjust
+        var newHeight = parseInt(getComputedStyle(sArr[i]).width) * 0.66;
+        newHeight = Math.round(newHeight);
+        sArr[i].setAttribute("style", "height: " + newHeight + "px;");
+      }
+      var _forEventListener = function() {
+        syAnimate(box, boxH, sH, sOffsetArrT, sImgArr);
+      }
+      syAnimate(box, boxH, sH, sOffsetArrT, sImgArr);
+    } // End if else row/column
     box.addEventListener("scroll", _forEventListener);
     window.addEventListener("resize", function() {
       box.removeEventListener("scroll", _forEventListener)
@@ -137,25 +107,8 @@ function imageSlider(firstRun) {
     window.addEventListener("orientationchange", function() {
       box.removeEventListener("scroll", _forEventListener)
     });
-    sAnimate(box, boxW, sW, sOffsetArr, sImgArr);
-    // Begin Desktop Specific
-    timeOut.push([j,"done"]); // create global timeOut variables for desktop nav
-    let target = j; // desktop nav target
-    let btnLeft = container.querySelector("div[class='btn-slider btn-slider-l']");
-    let btnRight = container.querySelector("div[class='btn-slider btn-slider-r']");
-    btnLeft.style.display = "none"; // initial hide left
-    let _forNavLeft = function() {
-      move(box, sWm, sImgArr, "left", btnLeft, btnRight, target);
-    }
-    let _forNavRight = function() {
-      move(box, sWm, sImgArr, "right", btnLeft, btnRight, target);
-    }
-    btnLeft.addEventListener("click", _forNavLeft);
-    btnRight.addEventListener("click", _forNavRight);
-    // End Desktop Specific
-  }
+  } // End for
 } //EOF
-
 // Init Slider
 window.addEventListener("load", function() {
   imageSlider(true);
